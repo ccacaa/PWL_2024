9aa028f85bf6f83c34477b6ac2327d4f83ad5e4a
Carieska Berliana Novita Kusuma Wardani <riskaberliana123@gmail.com>
2024-09-04 10:57:13 +0700

    pertemuan 2 prak2  Membuat Controller(7)

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+
 
 //Prak 1
 
@@ -55,4 +59,7 @@ use App\Http\Controllers\PageController;
 // Route::get('/about', [PageController::class,'about']);
 // Route::get('/articles/{id}', [PageController::class,'articles']);
 
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class,'about']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
 ?>
