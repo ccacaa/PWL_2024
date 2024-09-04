88e22d8233bd426e359c2f25f9934c836a0029e6
Carieska Berliana Novita Kusuma Wardani <riskaberliana123@gmail.com>
2024-09-04 07:35:48 +0700

    pertemuan 2 prak 1

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello World';
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
+
+Route::get('/world', function () {
+    return "World";
 });
+
 ?>
