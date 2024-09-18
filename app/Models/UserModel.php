ff1ce01cace42bc96796391b13f1eacc14bdbf87
Carieska Berliana Novita Kusuma Wardani <riskaberliana123@gmail.com>
2024-09-18 08:50:46 +0700

    js 4 prak 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,4 +10,10 @@ class UserModel extends Model
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    //js-4 prak 1 (1)
+    // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    //js-4 prak 1 (4)
+    protected $fillable = ['level_id', 'username', 'nama'];
 }
